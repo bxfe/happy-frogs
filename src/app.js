7779bdcd335cfb879e075672662e7924946d8a96
孙翛然 <c.salt@hotmail.com>
2017-10-27 21:21:57 +0800

    Add UI code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,41 @@
 import Vue from 'vue'
+import Frog from './component/frog'
+import Editor from './component/editor'
+import MessageBox from './component/message-box'
 
 new Vue({
   el: '#app',
-  template: '<p>{{message}}</p>',
+  components: {
+    'frog': Frog,
+    'editor': Editor,
+    'message-box': MessageBox,
+  },
   data: {
-    message: 'hello yp'
+    messages: [
+      {
+        type: 'frog',
+        content: '你好呀，我是小蛙',
+      },
+      {
+        type: 'user',
+        content: '你好呀，我是小蛙',
+      },
+      {
+        type: 'frog',
+        content: '你好呀，我是小蛙',
+      },
+    ],
+  },
+  methods: {
+    submit(content) {
+      this.messages.push({
+        type: 'user',
+        content,
+      })
+      this.messages.push({
+        type: 'frog',
+        content,
+      })
+    }
   }
 })

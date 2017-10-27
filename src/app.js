a07666c33ef055b8a2adfa1c48cd0b246fd9164a
杨攀 <yangpan@baixing.com>
2017-10-27 21:17:54 +0800

    add apiai

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import {ApiAiClient} from "api-ai-javascript";
 
+const client = new ApiAiClient({accessToken: 'b6111438a5f54e6eaa70e72aaab8d380'})
+
+.textRequest('Hello!')
+    .then((response) => { console.log(response)})
+    .catch((error) => {/* do something here too */})
 
 new Vue({
     el: '#app',

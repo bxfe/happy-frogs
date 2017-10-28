5672cf2bf2ae29bacedc4c680dd379e3a43f2bc1
孙翛然 <c.salt@hotmail.com>
2017-10-28 17:35:44 +0800

    Change server port to 80

--- server/main.js
+++ server/main.js
@@ -29,7 +29,7 @@ fastifyInstance.get('/textRequest', function(request, reply) {
     reply.type('application/json').send(res)
 })
 
-fastifyInstance.listen(3000, function (err) {
+fastifyInstance.listen(80, function (err) {
   if (err) throw err
   console.log(`server listening on ${fastifyInstance.server.address().port}`)
 })
